8ad32a7adae65f8030e1b464233ac83b2abbd5f9
EnriqueJose99 <enrique.galeano99@yahoo.com>
2018-01-30 08:18:11 -0600

    Fin porhoy

--- clase1.cpp
+++ clase1.cpp
@@ -19,45 +19,71 @@ void clearArray(int*);
 //imprime el contenido del arreglo
 void printArray(int*,int);
 
+//Read size
+int readSize();
+
 //main
 int main(){
+	int* array =NULL:
+	int size = 0;
+	int option = 0;
+	
+	while(option != 5){
+	    switch(option = menu()){
+                case 1:
+		    //crear arreglo
+		    //cout<<"Ingrese el tamano del arreglo: "<<endl;
+		    //cin>>size;
+		    size = readSize();
+		    //evitar memory leak
+		    if(array == NULL){
+		    	array = createArray(size);
+		    }else{
+		    	clearArray(array);
+			array = createArray(array);
+		    }
+		    break;
+		 case 5://salir del progama
+		    if(array != NULL)
+			    clearArray(array, size);
 
-
+	            break;
+		}
+	}
 	return 0;
 }
+
+
+//Reads size
+int readSize(){
+    int retVal = 0;
+    while(true){
+        cout<<"Ingrese el tamano del arrelgo: "<<endl;
+	cin>> retVal;
+	if(retVal >= 1)
+	    return retVal;
+	else 
+	    cout<<"Ingrese un valor positivo mayor a 1 "<<endl;
+    }//endWhile
+}
 //muestra menu y valida entrada
-int menu(int num){
+int menu(){
 	
 	do{
 	int num =0;
 	cout<<"Ingrese un numero para entrar a la opciones: "<<endl;
-	cin>>num;
+	cin>>num;                    cin>>size;
+
 	cout<<"1.Crear Arreglo"<<endl;
 	cout<<"2.Llenar el Arreglo"<<endl;
 	cout<<"3.Imprimir el Arreglo"<<endl;
 	cout<<"4.Limpiar el Arreglo"<<endl;
 	cout<<"5.Salir"<<endl;
-	if(num == 1){
-		cout<<"crear el arreglo"<<endl;
+	if(num>=1 && num <=5)
+		return num;
 
-	}
-	if(num == 2){
-		cout<<"llenar el arreglo"<<endl;
-
-	}
-	if(num == 3){
-		cout<<"Imprimir el arreglo"<<endl;
-	}
-	if(num ==4){
-	
-		cout<<"Limpiar el arreglo"<<endl;
-	}
-	if(num == 5){
-		cout<<"Salir"<<endl;
-	
-	}
-
-	}while(num>=1 && num<=5);
+	}while(num<1 && num>5);
+	cout<<"Ingrese otra vez un numero: " <<endl;
 }
 
 //reservar memoria dinamica para un arreglo
