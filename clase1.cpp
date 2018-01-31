490054018954ab7757d06e06bdf31ccfbcf7c34d
EnriqueJose99 <enrique.galeano99@yahoo.com>
2018-01-31 08:11:19 -0600

    fin

--- clase1.cpp
+++ clase1.cpp
@@ -22,6 +22,10 @@ void printArray(int*,int);
 //Read size
 int readSize();
 
+//llenar arreglo
+int* fillArray(int*, int); 
+
+
 //main
 int main(){
 	int* array =NULL;
@@ -36,22 +40,45 @@ int main(){
 		    //cin>>size;
 		    size = readSize();
 		    //evitar memory leak
-		    if(array == NULL){
+		  if(array == NULL){
 		    	array = createArray(size);
 		    }else{
 		    	clearArray(array);
 			array = createArray(size);
 		    }
 		    break;
-		 case 5://salir del progama
+		case 2:
+		    fillArray(array, size);
+		    break;
+		case 3:
+		    printArray(array, size);
+		    break;
+		case 4:
+		    clearArray(array);
+ 		    array = NULL;
+		    size = 0;
+		    break;
+		case 5://salir del progama
 		    if(array != NULL)
 			    clearArray(array);
 
 	            break;
+
 		}
 	}
 	return 0;
 }
+
+//llenar arreglo
+void fillArray(int* array, int size){
+	int temp;
+	for(int i = 0; i <= size; i++){
+		cout<<"Posicion: " <<i<<"Ingrese valor: "<<endl;
+		cin>> array[i];
+	}
+
+}
+
 
 //Reads size
 int readSize(){
@@ -76,13 +103,18 @@ int menu(){
 		cout<<"5.Salir"<<endl;
 		cout<<"Ingrese un numero para entrar a la opciones: "<<endl;
 		cin>>num;
-		return num;
+		if(num >= 1 && num <=5){
+			return num;
+		}else{
+			cout<<"$$ INGRESE DE NUEVO EL NUMERO $$"<<endl;
+		}
 	}
 }
 
 //reservar memoria dinamica para un arreglo
 int* createArray(int size){
-	return new int[size];
+	int* retValue = new int[size];
+	return retValue;
 }
 
 //clear array, remove space from memory
@@ -94,8 +126,8 @@ void clearArray(int* array){
 
 //print elements of the array
 void printArray(int*array, int size){
-	for(int i= 0;i < size;i++){
-		cout<<""<<array[i]<<endl;
+	for(int i= 0;i<=size;i++){
+		cout<<" "<<array[i]<<endl;
 	
 	}
 	cout<<endl;
